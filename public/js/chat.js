6dcde98b9f42a81f8485cfdb85f9eb57b47e2283
AngelaRecchia <angela.recchia@outlook.com>
2021-12-30 14:57:14 +0100

    geolocation

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,3 +11,12 @@ form.onsubmit = (e) => {
     socket.emit('sendMsg', msg.value)
     msg.value = '' 
 }
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) return alert('Geolocation not supported by your browser')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords
+        socket.emit('sendLocation', {latitude, longitude})
+    })
+})
